2a7cc2d8b2b2111f51ec3b30032433f7b4fa7465
Christopher Briceño <82926376+ChristoperBA@users.noreply.github.com>
2022-07-23 19:50:34 -0600

    Project Upload

---
+++ ProjectDataStructure/src/main/java/com/mycompany/projectdatastructure/ProjectDataStructure.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.projectdatastructure;
+
+
+public class ProjectDataStructure {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
