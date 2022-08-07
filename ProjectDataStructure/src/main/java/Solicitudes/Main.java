cdf19e36b86088fc5ecfd7e71f2886f15a6abeaf
Anthony <anthonydfv17@gmail.com>
2022-08-07 13:20:38 -0600

    Actualizacion

--- ProjectDataStructure/src/main/java/Solicitudes/Main.java
+++ ProjectDataStructure/src/main/java/Solicitudes/Main.java
@@ -15,21 +15,21 @@ public class Main {
      */
     public static void main(String[] args) {
 
-//        ListaDobleC dc = new ListaDobleC();
-//        dc.insertar(new Solicitud("1", 0, "A", 0,"BRONCE", "Nueva"));
-//        dc.insertar(new Solicitud("1", 1, "B", 1,"ZAFIRO", "Nueva"));
-//        dc.insertar(new Solicitud("2", 2, "C", 2,"BRONCE", "Nueva"));
-//        dc.insertar(new Solicitud("3", 3, "D", 3,"ORO", "Nueva"));
-//        dc.insertar(new Solicitud("4", 4, "E", 4,"PLATA", "Nueva"));
-//        dc.insertar(new Solicitud("5", 5, "F", 5,"ZAFIRO", "Nueva"));
-//        dc.insertar(new Solicitud("6", 6, "G", 6,"ORO", "Nueva"));
-//        dc.insertar(new Solicitud("7", 7, "H", 7,"PLATA", "Nueva"));
-//        dc.insertar(new Solicitud("8", 8, "I", 8,"BRONCE", "Nueva"));
-//        dc.insertar(new Solicitud("9", 9, "J", 9,"ORO", "Nueva"));
-//        dc.insertar(new Solicitud("10", 10, "K", 10,"PLATA", "Nueva"));
-//        dc.insertar(new Solicitud("11", 11, "L", 11,"ZAFIRO", "Nueva"));
-//        dc.insertar(new Solicitud("12", 12, "M", 12,"ORO", "Nueva"));
-//        dc.insertar(new Solicitud("13", 13, "N", 13,"PLATA", "Nueva"));
+        ListaDobleC dc = new ListaDobleC();
+//        dc.insertar(new Solicitud("1", 0, "A", 0,"BRONCE", "Registrado"));
+//        dc.insertar(new Solicitud("1", 1, "B", 1,"ZAFIRO", "Registrado"));
+//        dc.insertar(new Solicitud("2", 2, "C", 2,"BRONCE", "Registrado"));
+//        dc.insertar(new Solicitud("3", 3, "D", 3,"ORO", "Registrado"));
+//        dc.insertar(new Solicitud("4", 4, "E", 4,"PLATA", "Registrado"));
+//        dc.insertar(new Solicitud("5", 5, "F", 5,"ZAFIRO", "Registrado"));
+//        dc.insertar(new Solicitud("6", 6, "G", 6,"ORO", "Registrado"));
+//        dc.insertar(new Solicitud("7", 7, "H", 7,"PLATA", "Registrado"));
+//        dc.insertar(new Solicitud("8", 8, "I", 8,"BRONCE", "Registrado"));
+//        dc.insertar(new Solicitud("9", 9, "J", 9,"ORO", "Registrado"));
+//        dc.insertar(new Solicitud("10", 10, "K", 10,"PLATA", "Registrado"));
+//        dc.insertar(new Solicitud("11", 11, "L", 11,"ZAFIRO", "Registrado"));
+//        dc.insertar(new Solicitud("12", 12, "M", 12,"ORO", "Registrado"));
+//        dc.insertar(new Solicitud("13", 13, "N", 13,"PLATA", "Registrado"));
 //        dc.imprimirList();
         Menu m = new Menu();
         m.setVisible(true);
--- ProjectDataStructure/src/main/java/Solicitudes/Menu.java
+++ ProjectDataStructure/src/main/java/Solicitudes/Menu.java
@@ -26,20 +26,20 @@ public class Menu extends javax.swing.JFrame {
         tablaSolicitudes.setModel(modelo);
 
         dc = new ListaDobleC();
-        dc.insertar(new Solicitud("1", 0, "A", 0, "BRONCE", "Nueva"));
-        dc.insertar(new Solicitud("1", 1, "B", 1, "ZAFIRO", "Nueva"));
-        dc.insertar(new Solicitud("2", 2, "C", 2, "BRONCE", "Nueva"));
-        dc.insertar(new Solicitud("3", 3, "D", 3, "ORO", "Nueva"));
-        dc.insertar(new Solicitud("4", 4, "E", 4, "PLATA", "Nueva"));
-        dc.insertar(new Solicitud("5", 5, "F", 5, "ZAFIRO", "Nueva"));
-        dc.insertar(new Solicitud("6", 6, "G", 6, "ORO", "Nueva"));
-        dc.insertar(new Solicitud("7", 7, "H", 7, "PLATA", "Nueva"));
-        dc.insertar(new Solicitud("8", 8, "I", 8, "BRONCE", "Nueva"));
-        dc.insertar(new Solicitud("9", 9, "J", 9, "ORO", "Nueva"));
-        dc.insertar(new Solicitud("10", 10, "K", 10, "PLATA", "Nueva"));
-        dc.insertar(new Solicitud("11", 11, "L", 11, "ZAFIRO", "Nueva"));
-        dc.insertar(new Solicitud("12", 12, "M", 12, "ORO", "Nueva"));
-        dc.insertar(new Solicitud("13", 13, "N", 13, "PLATA", "Nueva"));
+        dc.insertar(new Solicitud("1", 0, "A", 0, "BRONCE", "Registrado"));
+        dc.insertar(new Solicitud("1", 1, "B", 1, "ZAFIRO", "Registrado"));
+        dc.insertar(new Solicitud("2", 2, "C", 2, "BRONCE", "Registrado"));
+        dc.insertar(new Solicitud("3", 3, "D", 3, "ORO", "Registrado"));
+        dc.insertar(new Solicitud("4", 4, "E", 4, "PLATA", "Registrado"));
+        dc.insertar(new Solicitud("5", 5, "F", 5, "ZAFIRO", "Registrado"));
+        dc.insertar(new Solicitud("6", 6, "G", 6, "ORO", "Registrado"));
+        dc.insertar(new Solicitud("7", 7, "H", 7, "PLATA", "Registrado"));
+        dc.insertar(new Solicitud("8", 8, "I", 8, "BRONCE", "Registrado"));
+        dc.insertar(new Solicitud("9", 9, "J", 9, "ORO", "Registrado"));
+        dc.insertar(new Solicitud("10", 10, "K", 10, "PLATA", "Registrado"));
+        dc.insertar(new Solicitud("11", 11, "L", 11, "ZAFIRO", "Registrado"));
+        dc.insertar(new Solicitud("12", 12, "M", 12, "ORO", "Registrado"));
+        dc.insertar(new Solicitud("13", 13, "N", 13, "PLATA", "Registrado"));
 
         dc.llenarTabla(modelo);
     }
@@ -239,6 +239,7 @@ public class Menu extends javax.swing.JFrame {
     }//GEN-LAST:event_tablaSolicitudesMouseClicked
 
     private void btnFinalizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFinalizarActionPerformed
+        text.setText("");
         dc.finalizarEstado(txtOculta.getText());
         dc.llenarTabla(modelo);
     }//GEN-LAST:event_btnFinalizarActionPerformed
